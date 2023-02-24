d76ace4185acc5b30b13f11e253deb721e3c790d
Daniel Dubinsky <daniel@quris.ai>
2023-02-24 17:28:40 +0200

    add task id

--- enq/enq.py
+++ enq/enq.py
@@ -10,23 +10,23 @@ import copy
 queue_file_path = Path(os.path.expanduser("~/.enq.json"))
 lock_file_path = Path(os.path.expanduser("~/.enq.json.lock"))
 flag = "--bg1234#$"
-interval = 5
+interval = 2
 
 
 def pop_queue():
     lock = FileLock(lock_file_path)
     lock.acquire()
     with open(queue_file_path, "r") as f:
-        queue: list = json.load(f)
-
+        data: dict = json.load(f)
+    queue = data["queue"]
     if len(queue) == 0:
-        return None
+        return None, 0
 
     n = queue.pop(0)
     with open(queue_file_path, "w") as f:
-        f.write(json.dumps(queue))
+        f.write(json.dumps(data, indent=4))
     lock.release()
-    return n
+    return n, len(queue)
 
 
 def try_to_enqueue():
@@ -36,14 +36,20 @@ def try_to_enqueue():
     lock = FileLock(lock_file_path)
     lock.acquire()
     with open(queue_file_path, "r") as f:
-        queue: list = json.load(f)
-
-    queue.append(sys.argv[1:])
+        data: dict = json.load(f)
+    queue = data["queue"]
+    id = data["last_id"]
+    element = {
+        "args": sys.argv[1:],
+        "id": id,
+    }
+    data["last_id"] += 1
+    queue.append(element)
     with open(queue_file_path, "w") as f:
-        f.write(json.dumps(queue))
+        f.write(json.dumps(data, indent=4))
     lock.release()
 
-    print("enqueued")
+    print(f"enqueued id {id}")
     return
 
 
@@ -73,7 +79,7 @@ if not lock_file_path.exists() or not queue_file_path.exists():
     with open(lock_file_path, "w") as f:
         f.write("lock")
     with open(queue_file_path, "w") as f:
-        f.write(json.dumps([]))
+        f.write(json.dumps({"last_id": 0, "queue": []}))
 else:
     try_to_enqueue()
     exit(0)
@@ -84,25 +90,31 @@ try_to_enqueue()
 class Manager:
     def __init__(self) -> None:
         self._num_devices = torch.cuda.device_count()
+        # self._num_devices = 2
+        if self._num_devices == 0:
+            raise Exception("No gpus found")
+
         self._available = [True for _ in range(self._num_devices)]
         self._running = [None for _ in range(self._num_devices)]
 
-        print(f"Initializing queue for {self._num_devices} devices")
+        print(f"Initializing queue for {self._num_devices} devices pid: {os.getpid()}")
 
     def available_index(self):
         if True in self._available:
             return self._available.index(True)
         return None
 
-    def launch_proc(self, idx, args):
+    def launch_proc(self, idx, element):
         self._available[idx] = False
+        args = element["args"]
+        id = element["id"]
         assert self._running[idx] == None
         args[0] = shutil.which(args[0])
-        print(f"running {args} on {idx}")
+        print(f"running id {id}:{args} on {idx}")
         env = copy.deepcopy(os.environ)
         env["CUDA_VISIBLE_DEVICES"] = f"{idx}"
         p = subprocess.Popen(args, cwd=os.getcwd(), start_new_session=True, env=env)
-        self._running[idx] = {"proc": p, "args": args}
+        self._running[idx] = {"proc": p, "args": args, "id": id}
 
     def poll_processes(self):
         for i, proc_dict in enumerate(self._running):
@@ -112,7 +124,7 @@ class Manager:
             is_running = ret_code is None
             if is_running:
                 continue
-            print(f'{proc_dict["args"]} finished running, returned {ret_code}')
+            print(f'id {proc_dict["id"]} finished running, returned {ret_code}')
             self._available[i] = True
             self._running[i] = None
 
@@ -124,13 +136,14 @@ class Manager:
                 time.sleep(interval)
                 continue
 
-            nxt = pop_queue()
+            nxt, n_left_in_queue = pop_queue()
             if nxt is None:
                 print("queue is empty")
                 reset()
                 return
-            print("popped", nxt)
             self.launch_proc(available_idx, nxt)
+            if self.available_index() is not None and n_left_in_queue > 0:
+                continue
             time.sleep(interval)
 
 
